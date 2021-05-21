cbdf195766f08c94fa371310566d5008ccb73be9
ryota-mitarai <ryotamitarai0@gmail.com>
2021-05-21 19:37:33 -0400

    feat: return button

--- src/webportal.js
+++ src/webportal.js
@@ -12,6 +12,7 @@ AFRAME.registerComponent('web-portal', {
     frameWidth: { default: 0.15 },
     portalFrame: { default: true },
     portalWebsurface: { default: true },
+    enableReturnButton: { default: true },
   },
 
   init: function () {
@@ -42,8 +43,68 @@ AFRAME.registerComponent('web-portal', {
       iframe.style.width = '100%';
       iframe.style.height = '100%';
       iframe.style.overflow = 'none';
-      iframe.style.zIndex = -10;
+      iframe.style.zIndex = 10;
       iframe.style.display = 'none';
+    }
+
+    if (data.enableReturnButton == true) {
+      const button = document.createElement('button');
+      button.innerHTML = 'return';
+
+      const css = `
+      button {
+        position: fixed;
+        top: .5em;
+        left: .5em;
+
+        cursor: pointer;
+        color: white;
+        box-shadow: 1px 1px 1px 1px #000000;
+        background-color: transparent;
+        border-radius: 5px;
+        border: 2px solid white;
+        
+        font-family: Arial;
+        font-size: 1em;
+        font-weight: bold;
+        text-shadow: 2px 2px 1px #000000;
+        padding: 0.25em 0.5em;
+
+        z-index: 20;
+      }
+      
+      button:hover {
+        color: lightgrey;
+        border-color: lightgrey;
+      }`;
+
+      const style = document.createElement('style');
+      if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+      } else {
+        style.appendChild(document.createTextNode(css));
+      }
+      button.appendChild(style);
+      document.body.appendChild(button);
+
+      button.style.display = 'none';
+
+      button.onclick = () => {
+        if (data.portalWebsurface == true) {
+          const context = el.css3d_context.domElement;
+
+          iframe.style = data.style_iframe;
+          context.style = data.style_context;
+          context.children[0].style.cssText = data.style_contextChild;
+
+          el.components['websurface'].play();
+        }
+
+        scene.style.display = 'block';
+        button.style.display = 'none';
+      };
+
+      data.returnButton = button;
     }
 
     var player;
@@ -92,15 +153,15 @@ AFRAME.registerComponent('web-portal', {
     }
 
     el.addEventListener('hitstart', function () {
-      if (data.portalWebsurface) {
-        el.pause();
+      if (data.portalWebsurface == true) {
+        el.components['websurface'].pause();
 
         iframe = el.websurface_iframe;
         const context = el.css3d_context.domElement;
 
-        data.style_iframe = iframe.style;
-        data.style_context = context.style;
-        data.style_contextChild = context.children[0].style;
+        data.style_iframe = iframe.style.cssText;
+        data.style_context = context.style.cssText;
+        data.style_contextChild = context.children[0].style.cssText;
 
         iframe.style = '';
         context.style = '';
@@ -114,10 +175,9 @@ AFRAME.registerComponent('web-portal', {
         iframe.style.overflow = 'none';
       }
 
-      scene.style.zIndex = -10;
       scene.style.display = 'none';
-      iframe.style.zIndex = 10;
       iframe.style.display = 'block';
+      if (data.returnButton) data.returnButton.style.display = 'block';
 
       document.exitPointerLock();
     });
